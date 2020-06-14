b6ea3d79ddfeb9c3f8496d28ad0284861731eda1
Lucas Ariel <lucasarieln@outlook.com>
2020-06-13 22:40:47 -0300

    Segunda alteração

---
+++ js/main.js
@@ -0,0 +1,131 @@
+
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+
+    //alert("Obrigado por clicar");
+}
+
+function redirecionar(){
+
+    //window.open("https://www.google.com.br");
+    //window.location.href = "https://www.google.com.br";
+}
+
+function trocar(){
+    document.getElementById("mousemove").innerHTML = "Passou o mouse.";
+    //alert("Trocar texto");
+}
+
+function voltar(){
+
+    document.getElementById("mousemove").innerHTML = "Passe o mouse aqui"
+
+}
+
+
+// function soma(n1,n2){
+//     return n1 + n2;
+// }
+
+
+
+// function validaIdade(idade){
+
+//     var validar;
+//     if (idade >= 18){
+//         validar = true;
+//     }else{
+//         validar = false;
+//     }
+//     return validar;
+// }
+
+// var idade = prompt("Qual sua idade?");
+// console.log(validaIdade(idade));
+
+// alert(soma(5, 10));
+
+
+// var lista  = [nome: “rosa” , “cravo” , “tulipa”, “margarida”]
+// console.log(lista[2]);
+
+// var d = new Date();
+// alert(d.getDate());
+// alert(d.getHours());
+// alert(d.getMinutes());
+
+
+
+/*
+ var count;
+ for(count=1; count <= 5; count++){
+     alert(count);
+ };
+*/
+
+
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+};
+*/
+
+/*
+prompt = prompt("Qual sua idade?");
+
+var idade = prompt;
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("Menor de idade");
+};
+
+*/
+
+// var frutas = [{nome:"maça", cor:"vermelho"}, 
+// {nome:"uva",cor:"roxa"}];
+// console.log(frutas);
+// console.log(frutas[1].nome);
+
+
+// var fruta = {nome:"maça", cor:"vermelho"}
+// console.log(fruta.nome);
+// console.log(fruta.cor);
+
+
+
+// var lista = ["maça", "pêra", "laranja"];
+// lista.push("uva")
+//console.log(lista);
+//lista.pop();
+
+
+
+
+// console.log(lista.toString());
+// console.log(lista.join(" - "));
+
+
+
+//console.log(lista[0]);
+//console.log(lista.toString()[0]);
+
+
+/*
+var nome = "Lucas Ariel";
+var n1 = 5;
+var n2 = 3;
+var frase = "Esta é uma frase para teste, oi teste!"
+//alert(nome + " tem " + idade + " anos.");
+//alert(idade + idade2);
+console.log(nome);
+console.log(n1*n2);
+//console.log(frase.toLocaleLowerCase())
+
+*/
+
